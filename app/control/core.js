62fb58a031cfa582dbda6b501bc55adb731ea9a3
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-10-03 10:09:59 -0300

    #

--- app/control/core.js
+++ app/control/core.js
@@ -1,3 +1,11 @@
 module.exports.index = function(req, res){
   res.render('core/index.ejs');
 }
+
+module.exports.contact = function(req, res){
+  res.render('core/contact.ejs');
+}
+
+module.exports.categories = function(req, res){
+  res.render('core/categories.ejs');
+}
--- app/routs/core.js
+++ app/routs/core.js
@@ -1,5 +1,15 @@
 module.exports = function(application){
+
   application.get('/', function(req, res){
     application.app.control.core.index(req, res);
   });
+
+  application.get('/contact', function(req, res){
+    application.app.control.core.contact(req, res);
+  });
+
+  application.get('/categories', function(req, res){
+    application.app.control.core.categories(req, res);
+  });
+
 }
