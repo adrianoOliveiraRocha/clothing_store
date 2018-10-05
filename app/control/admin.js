1f23bd9554fb6e45c363c2e726593cf461e78e93
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-10-05 12:56:43 -0300

    #

--- app/control/admin.js
+++ app/control/admin.js
@@ -4,9 +4,37 @@ module.exports.login = function(req, res){
 
 module.exports.register = function (req, res, application) {
   if (req['method'] == 'GET') {
-    res.render('admin/register.ejs');    
+    res.render('admin/register.ejs', { data: {}, validation: {} });    
   } else {
-    // var data = req.body;
-    res.send('data');  
+    var data = req.body;
+    req.assert('name', 'Nome é obrigatório').notEmpty();
+    req.assert('email', 'Email é obrigatório').notEmpty();
+    req.assert('email', 'Por favor, digite seu email corretamente').isEmail();
+    req.assert('pwd', 'Senha é obrigatório').notEmpty(); 
+    if (data.cpf) {
+      req.assert('cpf', 'O CPF deve ter 11 números').isLength({ min: 11 });
+      req.assert('cpf', 'O CPF deve ter 11 números').isLength({ max: 11 });  
+    }    
+    req.assert('pwd2', 'Por favor, digite a senha novamente').notEmpty();   
+    var errors = req.validationErrors();
+        
+    if (errors) {
+      res.render('admin/register.ejs', { data: data, validation: errors });      
+    } else {
+      if (data.pwd == data.pwd2) {
+        res.send('ok');
+      } else {// the passwords are differents
+        let pwd2 = [
+          {
+            location: 'body',
+            location: 'pwd2',
+            msg: 'As senhas são diferentes. Por favor, tente novamente',
+            value: data.pwd2
+          },
+        ]
+        res.render('admin/register.ejs', { data: data, validation: pwd2 });        
+      }
+    }   
+    
   }
 }
